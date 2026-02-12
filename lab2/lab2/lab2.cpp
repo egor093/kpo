e80cfb01abc89b485e5fb9624afbf82648ab8b6d
egor093 <99593834+egor093@users.noreply.github.com>
2026-02-12 16:48:18 +0300

    Add files via upload

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,30 @@
+﻿//#include <iostream>
+//#include <Windows.h>
+//
+//int main()
+//{
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//    int n = 13;
+//
+//    bool tr = true;
+//    bool fal = false;
+//
+//    char simbolLat = 'i';
+//    char sinbolRus = 'ы';
+//
+//    wchar_t wSimbolLat = 'l';
+//    wchar_t wSimbolRus = 'з';
+//
+//    short x = 9 + n;
+//    short minx = -(9 + n);
+//
+//    int y = 10 + n;
+//    int miny = (10 + n);
+//
+//    long z = 11 + n;
+//    long minz = -(11 + n);
+//
+//    float s = 1.0 + n;
+//    float mins = (1.0 + n);
+//}
